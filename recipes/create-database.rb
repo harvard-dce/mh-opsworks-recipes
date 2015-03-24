ef702ea7eee61315ded51edf068ee18c1d9b1571
Dan Collis-Puro <dan@collispuro.net>
2015-03-24 12:17:05 -0400

    Make database creation idempotent

--- recipes/create-database.rb
+++ recipes/create-database.rb
@@ -11,6 +11,11 @@ password = db_info[:password]
 port = db_info[:port]
 database_name = db_info[:database]
 
+database_connection = %Q|/usr/bin/mysql --user="#{username}" --host="#{host}" --port=#{port} --password="#{password}" "#{database_name}"|
+create_database = %Q|#{database_connection} < #{matterhorn_repo_root}/#{db_seed_file}|
+tables_exist = %Q(#{database_connection} -B -e "show tables" | grep -qie "Tables_in_#{database_name}")
+
 execute 'Create database' do
-  command %Q|/usr/bin/mysql --user="#{username}" --host="#{host}" --port=#{port} --password="#{password}" "#{database_name}" < #{matterhorn_repo_root}/#{db_seed_file}|
+  command create_database
+  not_if tables_exist
 end
