ebd2824096322f79d1fbd739595f189b451b04c3
Dan Collis-Puro <dan@collispuro.net>
2015-05-06 16:01:14 -0400

    Use local workspace to cache large uploaded HTTP body parts.

--- recipes/configure-nginx-proxy.rb
+++ recipes/configure-nginx-proxy.rb
@@ -3,10 +3,29 @@
 
 package 'nginx'
 
+storage_info = node.fetch(
+  :storage, {
+    export_root: '/var/tmp',
+    network: '10.0.0.0/8',
+    layer_shortname: 'storage'
+  }
+)
+export_root_parts = storage_info[:export_root].split('/')
+
+client_body_temp_path = (export_root_parts + [ 'nginx', 'client_body_temp' ]).join('/')
+
+directory client_body_temp_path do
+  owner 'www-data'
+  group 'www-data'
+  mode '755'
+  recursive true
+end
+
 template %Q|/etc/nginx/sites-enabled/default| do
   source 'nginx-proxy.conf.erb'
   variables({
-    matterhorn_backend_http_port: 8080
+    matterhorn_backend_http_port: 8080,
+    client_body_temp_path: client_body_temp_path
   })
 end
 
