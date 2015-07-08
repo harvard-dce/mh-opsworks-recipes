4b807b2e44a972a42b294c56e944b314a07f2a56
Dan Collis-Puro <dan@collispuro.net>
2015-07-08 09:45:52 -0400

    Install pv to help debug speed issues

--- recipes/install-utils.rb
+++ recipes/install-utils.rb
@@ -3,7 +3,7 @@
 
 include_recipe "mh-opsworks-recipes::update-package-repo"
 
-%w|htop nmap traceroute silversearcher-ag screen tmux iotop emacs24-nox mytop|.each do |package_name|
+%w|htop nmap traceroute silversearcher-ag screen tmux iotop emacs24-nox mytop pv|.each do |package_name|
   package package_name
 end
 
