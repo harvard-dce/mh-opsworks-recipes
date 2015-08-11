68b893d01c0179929751aecb48d0619db016474c
Dan Collis-Puro <dan@collispuro.net>
2015-08-11 15:19:24 -0400

    Fix any half-configured packages

--- recipes/update-package-repo.rb
+++ recipes/update-package-repo.rb
@@ -1,6 +1,14 @@
 # Cookbook Name:: mh-opsworks-recipes
 # Recipe:: update-package-repo
 
+execute 'fix any half-configured packages' do
+  environment 'DEBIAN_FRONTEND' => 'noninteractive'
+  command 'dpkg --configure -a'
+  timeout 30
+  retries 5
+  retry_delay 15
+end
+
 execute 'update package repository' do
   environment 'DEBIAN_FRONTEND' => 'noninteractive'
   command 'apt-get update'
