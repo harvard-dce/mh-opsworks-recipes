1d37338017978f83f0c58d492fb250cfed59d60f
Dan Collis-Puro <dan@collispuro.net>
2015-08-17 16:42:57 -0400

    Download and untar maven cache in one step
    The chef `remote_file` resource would frequently get never ending
    timeouts while downloading the maven cache tarball. Bring this in to
    timeout and retry the download to avoid stuck provisionings.

--- recipes/populate-maven-cache.rb
+++ recipes/populate-maven-cache.rb
@@ -3,14 +3,10 @@
 
 bucket_name = node.fetch(:shared_asset_bucket_name, 'dce-matterhorn-assets')
 
-remote_file '/root/m2_cache.tgz' do
-  source %Q|http://s3.amazonaws.com/#{bucket_name}/maven_cache.tgz|
-  action :create_if_missing
-  backup false
-  ignore_failure true
-end
-
-execute 'unpack maven cache' do
-  command 'cd /root && tar xvfz m2_cache.tgz'
-  not_if { ::File.exists?('/root/.m2') || ! ::File.exists?('/root/m2_cache.tgz') }
+# Remo
+execute 'download and unpack maven cache' do
+  command %Q|cd /root && /usr/bin/timeout 45 /usr/bin/curl -s http://s3.amazonaws.com/#{bucket_name}/maven_cache.tgz -o m2_cache.tgz && /bin/tar xvfz m2_cache.tgz|
+  retries 5
+  retry_delay 15
+  not_if { ::File.exists?('/root/.m2') }
 end
