08187da0c1c756eb64008a56f5be0dd8f078c138
Dan Collis-Puro <dan@collispuro.net>
2015-08-19 13:32:53 -0400

    Tweak timeout settings for maven cache generation

--- recipes/populate-maven-cache.rb
+++ recipes/populate-maven-cache.rb
@@ -1,12 +1,16 @@
 # Cookbook Name:: mh-opsworks-recipes
 # Recipe:: populate-maven-cache
 
-bucket_name = node.fetch(:shared_asset_bucket_name, 'dce-matterhorn-assets')
+include_recipe "mh-opsworks-recipes::update-package-repo"
+::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
+install_package('curl')
 
-# Remo
+bucket_name = node.fetch(:shared_asset_bucket_name, 'mh-opsworks-shared-assets')
+
 execute 'download and unpack maven cache' do
-  command %Q|cd /root && /usr/bin/timeout 45 /usr/bin/curl -s http://s3.amazonaws.com/#{bucket_name}/maven_cache.tgz -o m2_cache.tgz && /bin/tar xvfz m2_cache.tgz|
-  retries 5
+  command %Q|cd /root && /bin/rm -Rf .m2/ && /usr/bin/curl --continue-at - --silent http://s3.amazonaws.com/#{bucket_name}/maven_cache.tgz --output m2_cache.tgz && /bin/tar xvfz m2_cache.tgz && rm m2_cache.tgz|
+  retries 10
   retry_delay 15
+  timeout 45
   not_if { ::File.exists?('/root/.m2') }
 end
