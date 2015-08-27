2e679167fab26036ee4e40a0bae70566d28baa4d
Dan Collis-Puro <dan@collispuro.net>
2015-08-27 10:41:32 -0400

    Install nethogs to debug network usage

--- recipes/install-utils.rb
+++ recipes/install-utils.rb
@@ -4,7 +4,7 @@
 ::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
 include_recipe "mh-opsworks-recipes::update-package-repo"
 
-%w|htop nmap traceroute silversearcher-ag screen tmux iotop emacs24-nox mytop pv|.each do |name|
+%w|htop nmap traceroute silversearcher-ag screen tmux iotop emacs24-nox mytop pv nethogs|.each do |name|
   install_package(name)
 end
 
