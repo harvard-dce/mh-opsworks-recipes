be4a9bfee3c8b70d94c6f84e5608cb18cb74d36f
Dan Collis-Puro <dan@collispuro.net>
2015-09-14 16:41:15 -0400

    Increase maven cache timeout

--- recipes/populate-maven-cache.rb
+++ recipes/populate-maven-cache.rb
@@ -8,7 +8,7 @@ bucket_name = node.fetch(:shared_asset_bucket_name, 'mh-opsworks-shared-assets')
 execute 'download and unpack maven cache' do
   command %Q|cd /root && /bin/rm -Rf .m2/ && aws s3 cp s3://#{bucket_name}/maven_cache.tgz . && /bin/tar xvfz maven_cache.tgz && rm maven_cache.tgz|
   retries 10
-  retry_delay 15
-  timeout 45
+  retry_delay 5
+  timeout 300
   not_if { ::File.exists?('/root/.m2') }
 end
