77091ed020f4bd72cd64996a8e6667121f4d2262
Dan Collis-Puro <dan@collispuro.net>
2015-10-15 14:42:56 -0400

    Centralize package installs to optimize for custom AMI

--- recipes/install-mh-base-packages.rb
+++ recipes/install-mh-base-packages.rb
@@ -4,13 +4,26 @@
 ::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
 include_recipe "mh-opsworks-recipes::update-package-repo"
 
-%w|maven2 openjdk-7-jdk openjdk-7-jre
-  gstreamer0.10-plugins-base gstreamer0.10-plugins-good gstreamer0.10-tools
-  gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly
-  libglib2.0-dev mysql-client gzip tesseract-ocr npm nodejs nodejs-dev
-  mediainfo curl|.each do |package_name|
-    install_package(package_name)
-  end
+%w|autofs5
+curl
+dkms
+gzip
+libglib2.0-dev
+maven2
+mediainfo
+mysql-client
+nodejs
+nodejs-dev
+npm
+openjdk-7-jdk
+openjdk-7-jre
+postfix
+python-pip
+rsyslog-gnutls
+run-one
+tesseract-ocr|.each do |package_name|
+  install_package(package_name)
+end
 
 link '/usr/bin/node' do
   to '/usr/bin/nodejs'
