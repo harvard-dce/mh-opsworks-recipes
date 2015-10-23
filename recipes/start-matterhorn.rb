871898273f11858347cb1768a46b5514e9ff3a63
Dan Collis-Puro <dan@collispuro.net>
2015-10-23 16:47:27 -0400

    Only start matterhorn if the service is there

--- recipes/start-matterhorn.rb
+++ recipes/start-matterhorn.rb
@@ -1,4 +1,7 @@
 # Cookbook Name:: mh-opsworks-recipes
 # Recipe:: start-matterhorn
 
-execute 'service matterhorn start'
+execute 'start matterhorn' do
+  command 'service matterhorn start'
+  only_if '[ -f /etc/init.d/matterhorn ]'
+end
