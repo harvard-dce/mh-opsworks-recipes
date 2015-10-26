a634ffb8911d026113aa152730d0f1ad4f91a29d
Dan Collis-Puro <dan@collispuro.net>
2015-10-26 16:57:01 -0400

    WIP - load a cluster seed file into a cluster

---
+++ files/default/load-seed-data.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+do_it=false
+shared_files_path=
+bucket_name=
+seed_file=
+
+while :; do
+  case $1 in
+    -x)
+      do_it=true
+      ;;
+    -p|--path)
+      shared_files_path=$2
+      shift 2
+      continue
+      ;;
+    -b|--bucket_name)
+      bucket_name=$2
+      shift 2
+      continue
+      ;;
+    -s|--seed_file)
+      seed_file=$2
+      shift 2
+      continue
+      ;;
+    *)
+      break
+  esac
+
+  shift
+done
+
+if ! $do_it; then
+  echo
+  echo "Usage: load-seed-data.sh -x -p <full path to shared matterhorn files> -b <shared asset bucket name> -s <seed file name>"
+  echo
+  echo " -x	Actually load the seed data"
+  echo " -p	the full path to the root directory of the matterhorn files, probably on the shared workspace"
+  echo " -b	the bucket name that contains the seed file"
+  echo " -s	the seed file name - probably cluster_seed.tgz"
+  echo
+  exit 1;
+fi
+
+if [ -z "$shared_files_path" ]; then
+  echo 'Please provide a path to the root directory for where the files are stored'
+  exit 1
+fi
+
+if [ -z "$bucket_name" ]; then
+  echo 'Please provide a bucket_name'
+  exit 1
+fi
+
+if [ -z "$seed_file" ]; then
+  echo 'Please provide a seed_file'
+  exit 1
+fi
+
+# TODO
+
+cd $shared_files_path
+
+aws s3 cp s3://"$bucket_name"/"$seed_file" .
+tar xvfz "$seed_file"
+rm "$seed_file"
+
+# TODO
+# Load database file
+
---
+++ recipes/load-seed-data.rb
@@ -0,0 +1,28 @@
+# Cookbook Name:: mh-opsworks-recipes
+# Recipe:: reset-database
+
+::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
+include_recipe "awscli::default"
+
+do_it = node.fetch(:do_it, false)
+shared_storage_root = get_shared_storage_root
+seed_file = get_seed_file
+bucket_name = get_shared_asset_bucket_name
+
+if testing_cluster?
+  cookbook_file 'load-seed-data.sh' do
+    path '/usr/local/bin/load-seed-data.sh'
+    owner 'root'
+    group 'root'
+    mode '700'
+  end
+
+  if do_it && database_node?
+    include_recipe 'mh-opsworks-recipes::remove-all-matterhorn-files'
+
+    execute 'load seed database and create seed files' do
+      user "root"
+      command %Q|/usr/local/bin/load-seed-data.sh -x -p "#{shared_storage_root}" -b "#{bucket_name}" -s "#{seed_file}"|
+    end
+  end
+end
