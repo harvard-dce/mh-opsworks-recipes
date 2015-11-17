4fd5c56c52f2f472d63a97231a38acd439cb0d67
Dan Collis-Puro <dan@collispuro.net>
2015-11-17 11:10:39 -0500

    Get domain, not URL

--- recipes/create-cluster-seed-file.rb
+++ recipes/create-cluster-seed-file.rb
@@ -8,9 +8,8 @@ shared_storage_root = get_shared_storage_root
 source_engage_ip = get_public_engage_ip
 source_engage_host = get_public_engage_hostname
 source_admin_ip = get_public_admin_ip
-source_cloudfront_domain = get_base_media_download_url(source_engage_host)
+source_cloudfront_domain = get_base_media_download_domain(source_engage_host)
 source_wowza_edge_url = get_live_streaming_url
-Chef::Log.info("From create-cluster-seed-file: source_cloudfront_domain: #{source_cloudfront_domain}")
 
 if testing_cluster?
   cookbook_file 'create-cluster-seed-file.sh' do
@@ -21,12 +20,9 @@ if testing_cluster?
   end
 
   if do_it && database_node?
-    run_command = %Q|/usr/local/bin/create-cluster-seed-file.sh -x -p "#{shared_storage_root}" --source_engage_ip "#{source_engage_ip}" --source_engage_host "#{source_engage_host}" --source_admin_ip "#{source_admin_ip}" --source_cloudfront_domain="#{source_cloudfront_domain}" --source_wowza_edge_url="#{source_wowza_edge_url}"|
-
-    Chef::Log.info("Command: #{run_command}")
     execute 'create cluster seed file' do
       user "root"
-      command run_command
+      command %Q|/usr/local/bin/create-cluster-seed-file.sh -x -p "#{shared_storage_root}" --source_engage_ip "#{source_engage_ip}" --source_engage_host "#{source_engage_host}" --source_admin_ip "#{source_admin_ip}" --source_cloudfront_domain "#{source_cloudfront_domain}" --source_wowza_edge_url "#{source_wowza_edge_url}"|
     end
   end
 end
