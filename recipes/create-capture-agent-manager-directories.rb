194ca39a4538778066c4eddb9ffc1dd238e4baab
nmaekawa <nmaekawa@g.harvard.edu>
2016-02-12 12:00:43 -0500

    matt-1875: recipes for capture_agent_manager webapp
    - create non-priviledged user
    - install python pkgs and supervisor
    - virtualenv and pip requirements
    - gunicorn start script virtualenv
    - prevent re-create virtualenv
    - redis
    - logrotate for app related logs
    - using refactor for certificates
    - (fix pull #81) more descriptive names
    - (fix pull #81) string of packages to `install-packages` recipe
    - (fix pull #81) redis default save to disk is enough for now
    - (fix pull #81) nginx configs
    - (fix pull #81) logrotate keeps 7 logs instead of 45
    - (fix pull #83) fix consistency slips
    - (fix pull #83) fix redundant command + config vars

---
+++ recipes/create-capture-agent-manager-directories.rb
@@ -0,0 +1,19 @@
+# Cookbook Name:: mh-opsworks-recipes
+# Recipe:: create-python-manager-directories
+
+::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
+
+usr_name = get_capture_agent_manager_usr_name
+
+[
+  %Q|home/#{usr_name}/sites|,
+  %Q|home/#{usr_name}/sock|,
+  %Q|home/#{usr_name}/logs|
+].each do |capture_agent_manager_directory|
+  directory capture_agent_manager_directory do
+    owner usr_name
+    group usr_name
+    mode "755"
+    recursive true
+  end
+end
