dddfa4331cee64aaa5b826306151f87e462de921
Dan Collis-Puro <dan@collispuro.net>
2016-04-13 14:08:17 -0400

    Fix the PATH for matterhorn @reboot starts

--- recipes/register-matterhorn-to-boot.rb
+++ recipes/register-matterhorn-to-boot.rb
@@ -6,6 +6,7 @@ cron_d 'start_matterhorn_at_boot' do
   # because the default matterhorn init causes other init scripts to fail to run during
   # a boot under the upstart sysvinit compatibility layer. Completely mysterious and
   # frustrating.
+  path '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
   predefined_value '@reboot'
   command '/etc/init.d/matterhorn start'
 end
