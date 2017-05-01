3ad2b94492a4ba2f36f3b071acf71810b460509a
Jay Luker <jay_luker@harvard.edu>
2017-05-01 09:25:00 -0400

    Updated to accomodate java 8 jstat output

--- files/default/jvm_metrics.sh
+++ files/default/jvm_metrics.sh
@@ -9,13 +9,14 @@ namespace="Java/$app_name"
 proc_id=`jps -l | grep $app_name | cut -d ' ' -f 1`
 data=`jstat -gcutil $proc_id | tail -1 | tr -s ' ' | cut -c2-100`
 
-echo $data | while read survivor0 survivor1 eden oldgen perm younggc younggctime fullgc fullgctime totalgctime
+echo $data | while read survivor0 survivor1 eden oldgen metaspace compclass younggc younggctime fullgc fullgctime totalgctime
 do
     aws cloudwatch put-metric-data --metric-name="Survivor0Usage" --value="$survivor0" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
     aws cloudwatch put-metric-data --metric-name="Survivor1Usage" --value="$survivor1" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
     aws cloudwatch put-metric-data --metric-name="EdenUsage" --value="$eden" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
     aws cloudwatch put-metric-data --metric-name="OldGenUsage" --value="$oldgen" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
-    aws cloudwatch put-metric-data --metric-name="PermUsage" --value="$perm" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
+    aws cloudwatch put-metric-data --metric-name="MetaspaceUsage" --value="$metaspace" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
+    aws cloudwatch put-metric-data --metric-name="CompressedClassUsage" --value="$compclass" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
     aws cloudwatch put-metric-data --metric-name="YoungGCCount" --value="$younggc" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
     aws cloudwatch put-metric-data --metric-name="YoungGCTime" --value="$younggctime" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Seconds --region="$region"
     aws cloudwatch put-metric-data --metric-name="FullGCCount" --value="$fullgc" --namespace="$namespace" --dimensions="InstanceId=$instance_id" --unit Percent --region="$region"
