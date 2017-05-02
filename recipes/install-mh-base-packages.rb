5efaa60bb5014f8025248fde25764ef190591f66
Jay Luker <jay_luker@harvard.edu>
2017-05-02 05:37:46 -0400

    jvm management stuff:
    * install yourkit jvm profiler
    * log garbage collection details
    * allow use of G1 garbage collector

--- recipes/install-mh-base-packages.rb
+++ recipes/install-mh-base-packages.rb
@@ -12,4 +12,5 @@ end
 packages = %Q|autofs5 curl dkms gzip jq libglib2.0-dev maven mediainfo mysql-client openjdk-7-jdk openjdk-7-jre postfix python-pip rsyslog-gnutls run-one tesseract-ocr|
 install_package(packages)
 
+include_recipe "mh-opsworks-recipes::install-yourkit-agent"
 include_recipe "mh-opsworks-recipes::clean-up-package-cache"
---
+++ recipes/install-yourkit-agent.rb
@@ -0,0 +1,25 @@
+# Cookbook Name:: mh-opsworks-recipes
+# Recipe:: install-yourkit
+
+::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
+
+matterhorn_repo_root = node[:matterhorn_repo_root]
+shared_assets_bucket = get_shared_asset_bucket_name
+agent_url = "https://s3.amazonaws.com/#{shared_assets_bucket}/libyjpagent.so"
+yourkit_dir = "#{matterhorn_repo_root}/yourkit"
+agent_filepath = "#{yourkit_dir}/libyjpagent.so"
+
+directory yourkit_dir do
+  owner 'matterhorn'
+  group 'matterhorn'
+  mode '0755'
+end
+
+remote_file agent_filepath do
+  source agent_url
+  action :create_if_missing
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
