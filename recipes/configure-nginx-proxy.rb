ca2566331825c5e4233fe5fa4b42c946bbb2e9cf
Karen Dolan <kdolan@dce.harvard.edu>
2017-06-19 15:25:24 -0400

    OPC-36 nginx big upload file temp cache loc to large shared storage

--- recipes/configure-nginx-proxy.rb
+++ recipes/configure-nginx-proxy.rb
@@ -16,17 +16,30 @@ template %Q|/etc/nginx/nginx.conf| do
   })
 end
 
+# save nginx temp big upload video block cache to Zadara array
+body_temp_path = get_shared_storage_root + "/nginx/body-temp"
+
 template %Q|/etc/nginx/sites-enabled/default| do
   source 'nginx-proxy.conf.erb'
   manage_symlink_source true
   variables({
-    opencast_backend_http_port: 8080
+    opencast_backend_http_port: 8080,
+    body_temp_path: body_temp_path
   })
 end
 
 directory '/etc/nginx/proxy-includes' do
   owner 'root'
   group 'root'
+end
+
+# same permissions as nginx logs
+directory  %Q|body_temp_path| do
+  action :create
+  owner 'www-data'
+  group 'admin'
+  mode '755'
+  recursive true
 end
 
 execute 'service nginx reload'
