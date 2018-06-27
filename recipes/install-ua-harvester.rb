7d57e452ff504bfc4369ca0ccdc3caabd5ec3e34
nataliemona <nataliemona@g.harvard.edu>
2018-06-27 15:36:14 -0400

    fixes

--- recipes/install-ua-harvester.rb
+++ recipes/install-ua-harvester.rb
@@ -35,8 +35,7 @@ bash 'install dependencies' do
 cd /home/ua_harvester/harvester &&
 /usr/bin/virtualenv venv &&
 source venv/bin/activate &&
-chown -R $USER /root/.cache/pip/wheels/
-pip install --user --no-cache-dir -r requirements.txt
+pip install --user -r requirements.txt
   |
   user 'ua_harvester'
 end
