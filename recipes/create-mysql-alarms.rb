abf6c77071173a4b5a18797c14d29b96e32a021d
Jay Luker <jay_luker@harvard.edu>
2018-09-05 11:11:14 -0400

    t/MI-107: rds alarm thresholds
    * set rds write iops alert threshold based on allocated storage
    * bump cpu usage threshold to something more reasonable (25% is way too
      conservative)

--- recipes/create-mysql-alarms.rb
+++ recipes/create-mysql-alarms.rb
@@ -10,7 +10,7 @@ ruby_block "fire alarms for MySQL RDS metrics" do
     region = 'us-east-1'
     topic_arn = execute_command(%Q(aws sns create-topic --name "#{topic_name}" --region #{region} --output text)).chomp
 
-    allocated_storage_in_gig = execute_command(%Q(aws rds describe-db-instances --db-instance-identifier "#{rds_name}" --region #{region} --output json | grep AllocatedStorage | sed "s/[^[:digit:]]//g")).chomp.to_i
+    allocated_storage_in_gig = execute_command(%Q(aws rds describe-db-instances --db-instance-identifier "#{rds_name}" --region #{region} --output text --query 'DBInstances[].AllocatedStorage')).chomp.to_i
 
     min_percent_free_space = 0.25
     allocated_storage_in_bytes = allocated_storage_in_gig * (1024 * 1024 * 1024)
@@ -21,8 +21,8 @@ ruby_block "fire alarms for MySQL RDS metrics" do
     Chef::Log.info command
     execute_command(command)
 
-    # CPU utilization over 25 for 5 1 minute periods
-    upper_cpu_percent = 25
+    # CPU utilization over 80 for 10 1 minute periods
+    upper_cpu_percent = 80
     command = %Q(aws cloudwatch put-metric-alarm --region "#{region}" --alarm-name "#{rds_name}_cpu_usage_high" --alarm-description "MySQL RDS cpu utilization is high" --metric-name CPUUtilization --namespace AWS/RDS --statistic Maximum --period 60 --threshold #{upper_cpu_percent} --comparison-operator GreaterThanThreshold --dimensions Name=DBInstanceIdentifier,Value=#{rds_name} --evaluation-periods 10 --alarm-actions "#{topic_arn}")
     Chef::Log.info command
     execute_command(command)
@@ -34,14 +34,19 @@ ruby_block "fire alarms for MySQL RDS metrics" do
     Chef::Log.info command
     execute_command(command)
 
-    # Write operations over the threshold for 5 1 minute periods. This is rather coarse and reactionary, but
-    # if there are too many writes the database is probably not responding well.
-    alert_on_write_iops_over = 140
+    # an instance's baseline iops is determined by the size of the underlying ebs volume.
+    # surpassing the baseline is fine for brief periods as the iops are "burstable", but
+    # you don't want to go over for too long, so we'll alert if we go over 80% of the baseline
+    baseline_iops = allocated_storage_in_gig * 3
+    alert_on_write_iops_over = (baseline_iops * 0.80).to_i
+
+    # Write operations over the threshold for 5 1 minute periods. This is rather coarse and reactionary, and
+    # lots of writes can be seen as the database performing *well*, so take this one in context
     command = %Q(aws cloudwatch put-metric-alarm --region "#{region}" --alarm-name "#{rds_name}_write_iops_high" --alarm-description "MySQL RDS write iops are high" --metric-name WriteIOPS --namespace AWS/RDS --statistic Maximum --period 60 --threshold #{alert_on_write_iops_over} --comparison-operator GreaterThanThreshold --dimensions Name=DBInstanceIdentifier,Value=#{rds_name} --evaluation-periods 5 --alarm-actions "#{topic_arn}")
     Chef::Log.info command
     execute_command(command)
 
-    # Queue depth is over 2 for 5 1 minute periods. This means IO processes are stacking
+    # Queue depth is over 2 for 10 1 minute periods. This means IO processes are stacking
     # up and EBS is getting swamped.
     disk_queue_depth_over = 2
     command = %Q(aws cloudwatch put-metric-alarm --region "#{region}" --alarm-name "#{rds_name}_queue_depth_high" --alarm-description "MySQL RDS EBS queue depth is high" --metric-name DiskQueueDepth --namespace AWS/RDS --statistic Maximum --period 60 --threshold #{disk_queue_depth_over} --comparison-operator GreaterThanThreshold --dimensions Name=DBInstanceIdentifier,Value=#{rds_name} --evaluation-periods 10 --alarm-actions "#{topic_arn}")
