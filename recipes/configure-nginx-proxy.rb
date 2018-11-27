81bc6da3ef0eba9f3aeeda1147f643dd3c2821d8
Jay Luker <jay_luker@harvard.edu>
2018-11-27 16:39:40 -0500

    Don't set client_body_temp_path; 5x uses a different upload mechanism

--- recipes/configure-nginx-proxy.rb
+++ recipes/configure-nginx-proxy.rb
@@ -9,18 +9,6 @@ install_nginx_logrotate_customizations
 
 worker_procs = get_nginx_worker_procs
 
-# The template path variable cannot be a get, must be saved locally first
-body_temp_path = get_nginx_body_temp_path
-
-# create path for nginx to buffer large uploads, the get can be called directly
-directory get_nginx_body_temp_path do
-  action :create
-  owner 'www-data'
-  group 'admin'
-  mode '755'
-  recursive true
-end
-
 directory '/etc/nginx/proxy-includes' do
   owner 'root'
   group 'root'
@@ -38,8 +26,7 @@ template 'proxy' do
   path %Q|/etc/nginx/conf.d/default.conf|
   source 'nginx-proxy.conf.erb'
   variables({
-    opencast_backend_http_port: 8080,
-    body_temp_path: body_temp_path
+    opencast_backend_http_port: 8080
   })
   notifies :restart, 'service[nginx]', :immediately
 end
