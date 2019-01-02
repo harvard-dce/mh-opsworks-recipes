6dc684dbbc26080be37e13daff0319c562bd60e8
Jay Luker <jay_luker@harvard.edu>
2019-01-02 05:41:25 -0500

    MI-140: index removal script now uses correct solr index path
    This script is used during the `cluster:reset` task.
    Also added removal of elasticsearch index data.

--- recipes/remove-engage-indexes.rb
+++ recipes/remove-engage-indexes.rb
@@ -6,17 +6,17 @@ do_it = node.fetch(:do_it, false)
 local_workspace_root = get_local_workspace_root
 
 if dev_or_testing_cluster?
-  cookbook_file 'remove-solr-indexes.sh' do
-    path '/usr/local/bin/remove-solr-indexes.sh'
+  cookbook_file 'remove-indexes.sh' do
+    path '/usr/local/bin/remove-indexes.sh'
     owner 'root'
     group 'root'
     mode '755'
   end
 
   if do_it && engage_node?
-    execute 'remove engage solr indexes' do
+    execute 'remove engage solr and elasticsearch indexes' do
       user 'opencast'
-      command %Q|/usr/local/bin/remove-solr-indexes.sh -x -p #{local_workspace_root}|
+      command %Q|/usr/local/bin/remove-indexes.sh -x -p #{local_workspace_root}|
     end
   end
 end
