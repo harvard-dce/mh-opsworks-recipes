badb109a620f8a892ce3966201ca41bdcba01cc3
Jay Luker <jay_luker@harvard.edu>
2021-08-02 14:42:09 -0400

    uninstall java 1.7 so that 1.8 becomes default

--- recipes/install-oc-base-packages.rb
+++ recipes/install-oc-base-packages.rb
@@ -9,8 +9,14 @@ include_recipe "oc-opsworks-recipes::update-package-repo"
 # make sure we get this one from the epel repo or it will cause dep conflicts with tesseract
 install_package("libwebp", %Q|--disablerepo="*" --enablerepo="epel"|)
 
-packages = %Q|java-1.8.0-openjdk autofs curl mysql56 postfix rsyslog-gnutls tesseract|
+packages = %Q|java-1.8.0-openjdk java-1.8.0-openjdk-devel autofs curl mysql56 postfix rsyslog-gnutls tesseract|
 install_package(packages)
+
+# remove java-1.7 so that 1.8 becomes default
+package 'java-1.7.0-openjdk' do
+  action :remove
+  ignore_failure true
+end
 
 include_recipe 'maven'
 
