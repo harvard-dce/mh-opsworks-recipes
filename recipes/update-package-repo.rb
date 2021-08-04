bf8693c0675e37c7a476fd6989cc1bf8fdab3889
Jay Luker <jay_luker@harvard.edu>
2021-08-04 10:06:40 -0400

    fixing the yum package list refresh command

--- recipes/update-package-repo.rb
+++ recipes/update-package-repo.rb
@@ -7,8 +7,11 @@ execute 'enable epel' do
   retry_delay 15
 end
 
-execute 'update package repository' do
-  command 'yum update'
+# not strictly necessary as the standard yum install commands will refresh
+# the package list when needed, but this might save a time when many packages
+# are being installed sequentially
+execute 'update package list' do
+  command 'yum makecache'
   timeout 180
   retries 5
   retry_delay 15
