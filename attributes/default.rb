a63246c118a5d41cf6afae914b3b16f21e5f4529
Karen Dolan <karen_dolan@harvard.edu>
2022-05-17 11:20:20 -0400

    test maven built lti and admin-ui

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 
 # Maven
-default['maven']['version'] = '3.3.9'
+default['maven']['version'] = '3.8.1'
 default['maven']['setup_bin'] = true
 default['maven']['mavenrc']['opts'] = '-Dmaven.repo.local=/root/.m2/repository -Xms1024m -Xmx1024m'
 
